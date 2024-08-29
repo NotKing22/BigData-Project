9fa7506f922d9aa9075abb960837f6ecae1b6342
Everton Mutti <evertoncastro19@gmail.com>
2024-08-28 21:30:35 -0300

    feat: project skeleton

--- project/app/main.py
+++ project/app/main.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import dash
+from project.app.settings.settings import get_settings
 
+settings = get_settings()
 
 df_position = pd.read_csv('app/data/job_skills.csv', nrows=200)
 df_summary = pd.read_csv('app/data/job_summary.csv', nrows=200)
@@ -10,4 +12,4 @@ df_skills = pd.read_csv('app/data/job_skills.csv', nrows=200)
 app = dash.Dash(__name__)
 
 if __name__ == '__main__':
-    app.run_server(debug=True if )
+    app.run_server(debug=True if settings.env_settings.env == 'DEV' else False)
